451df343998a16dce044fd538797b8ee0ddef3bd
BakiAbdullah <shatil605@gmail.com>
2023-11-19 08:45:05 +0600

    basic-project setup done

---
+++ src/app.ts
@@ -0,0 +1,15 @@
+// const express = require("express");
+import express, { Application, Request, Response } from 'express'
+import cors from 'cors'
+const app: Application = express()
+
+// Parser
+app.use(express.json())
+app.use(cors())
+
+app.get('/', (req: Request, res: Response) => {
+  const a = 10
+  res.send(a)
+})
+
+export default app
