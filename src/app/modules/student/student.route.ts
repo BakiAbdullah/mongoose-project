2ba70628058cd9f3d5cf2d49384a4d816d596fb1
BakiAbdullah <shatil605@gmail.com>
2023-11-26 11:25:08 +0600

    structured some files

--- src/app/modules/student/student.route.ts
+++ src/app/modules/student/student.route.ts
@@ -4,11 +4,8 @@ import { StudentControllers } from './student.controller';
 const router = express.Router();
 
 // route will call controller functions
-
 router.post('/create-student', StudentControllers.createStudent)
-
 router.get('/', StudentControllers.getAllStudents)
-
 router.get('/:studentId', StudentControllers.getSingleStudent)
 
 export const studentRoutes = router
